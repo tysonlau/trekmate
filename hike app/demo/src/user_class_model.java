9b82657f3f298b1f0cd09c02bd76badb1c18736e
Kristina Tretiak <73917302+KrisTretiak@users.noreply.github.com>
2023-06-30 13:01:12 -0700

    Add files via upload

---
+++ hike app/demo/src/user_class_model.java
@@ -0,0 +1,55 @@
+
+
+
+public class User {
+    private String id;
+    private String name;
+    private int age;
+    private String password;
+
+    // Constructors, getters, and setters
+
+    public User() {
+    }
+
+    public User(String id, String name, int age, String password) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.password = password;
+    }
+
+    // Getters and setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
