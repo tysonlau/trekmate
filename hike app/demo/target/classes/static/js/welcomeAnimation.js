99698ec27a3c15f46eda85be4822e446166c6a36
Henry Dinh <dinhhenry@hotmail.com>
2023-07-03 17:51:35 -0700

    completed pages; need to link to controllers

--- hike app/demo/target/classes/static/js/welcomeAnimation.js
+++ hike app/demo/target/classes/static/js/welcomeAnimation.js
@@ -2,44 +2,41 @@ let login = document.querySelector(".loginButton");
 let register = document.querySelector(".registerButton");
 let loginForm = document.querySelector(".login-form");
 let registerForm = document.querySelector(".register-form");
-let goBack = document.querySelectorAll(".return");
+let goBack = document.querySelector(".return");
 
 var loginTrue = false;
 var registerTrue = false;
+var buttonsShow = false;
 
-login.addEventListener("click", ()=>{
+login.addEventListener("click", showForm);
+
+function showForm(){
+    if(buttonsShow == true){
+        login.classList.remove("loginButton-show");
+        register.classList.remove("registerButton-show");
+    }
+    
     loginForm.classList.add("login-form-move");
     login.classList.add("loginButton-hide");
     register.classList.add("registerButton-hide");
-    goBack[0].classList.add("return-visible");
-    loginTrue = true;
-})
+    loginTrue = true;  
+}
 
-register.addEventListener("click", ()=>{
-    registerForm.classList.add("register-form-move");
-    login.classList.add("loginButton-hide");
-    register.classList.add("registerButton-hide");
-    goBack[1].classList.add("return-visible");
-    registerTrue = true;
-})
-
-goBack[0].addEventListener("click", returning);
-goBack[1].addEventListener("click", returning);
+goBack.addEventListener("click", returning);
 
 function returning(event){
     event.preventDefault();
     if (loginTrue == true){
         loginForm.classList.remove("login-form-move");
-        goBack[0].classList.remove("return-visible");
         loginTrue = false;
     }
 
     if (registerTrue == true){
         registerForm.classList.remove("register-form-move");
         registerTrue = false;
-        goBack[1].classList.remove("return-visible");
     }
 
-    login.classList.remove("loginButton-hide");
-    register.classList.remove("registerButton-hide");
+    login.classList.replace("loginButton-hide", "loginButton-show");
+    register.classList.replace("registerButton-hide","registerButton-show");
+    buttonsShow = true;
 }
